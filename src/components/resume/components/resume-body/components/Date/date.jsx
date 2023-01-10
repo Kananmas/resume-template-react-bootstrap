3c2891c7d9a3724963036a2b871d131b08f6bbfb
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-10 22:15:12 +0330

    Update date.jsx

--- src/components/resume/components/resume-body/components/Date/date.jsx
+++ src/components/resume/components/resume-body/components/Date/date.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
+const style = { fontSize: "16px" };
 export default function Date(props) {
   return (
     <div className="my-1">
-      <code className="text-dark" style={{ fontSize: "16px" }}>
+      <code className="text-dark" style={style}>
         {props.StartingDate}
       </code>
       -
-      <code className="text-dark" style={{ fontSize: "16px" }}>
+      <code className="text-dark" style={style}>
         {props.EndingDate}
       </code>
     </div>
