2f00daf761ac4f09e57c8462b727ca9f63eaa3a0
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-10 16:24:26 +0330

    Delete src directory

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./scripts/main";
-import $Kanan from "./utlis/my.info";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App person={$Kanan} />);
