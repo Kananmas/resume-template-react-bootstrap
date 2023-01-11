53677d8c2e2b20ca5090f4bb92fae602a40ec6fb
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-11 15:14:22 +0330

    Update index.jsx

--- src/components/resume/components/resume-body/index.jsx
+++ src/components/resume/components/resume-body/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SkillsandReferences from "./components/SkillandRefrences/skills-and-refrences";
-import { EmploymenthistotyEducations } from "./components/Background/background";
+import { Background } from "./components/Background/background";
 
 export function Resumebody(props) {
   return (
     <div className="body my-2">
       <div className="row">
-        <EmploymenthistotyEducations
+        <Background
           educations={props.educations}
           workhistory={props.workhistory}
         />
